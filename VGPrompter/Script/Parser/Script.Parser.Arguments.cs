7f01dd6e12055914e6b50a1f7add5104edc66924
eugeniusfox <eugeniusfox@gmail.com>
2018-01-03 20:53:02 +0100

    Add new lexer

---
+++ VGPrompter/Script/Parser/Script.Parser.Arguments.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace VGPrompter {
+
+    public partial class Script {
+
+        public static partial class Parser {
+
+            struct Arguments {
+
+                public static Regex identifier_re = new Regex("^[A-z][A-z0-9_]*$", RegexOptions.Compiled);
+
+                public List<object> PositionalArguments { get; private set; }
+                public Dictionary<string, object> KeywordArguments { get; private set; }
+
+                public Arguments(List<object> args, Dictionary<string, object> kwargs) : this() {
+                    PositionalArguments = args;
+                    KeywordArguments = kwargs;
+                }
+
+                public static Arguments Parse(string s) {
+                    var args = new List<object>();
+                    var kwargs = new Dictionary<string, object>();
+
+                    var argv = s.Split(',').Select(a => a.Trim());
+
+                    foreach (var a in argv) {
+                        if (a.Contains('=')) {
+                            var kvp = a.Split('=').Select(t => t.Trim()).ToArray();
+                            if (!identifier_re.Match(kvp[0]).Success) throw new Exception(string.Format("Invalid key '{0}'!", kvp[0]));
+                            kwargs.Add(kvp[0], kvp[1]);
+                        } else {
+                            if (kwargs.Count > 0) throw new Exception("!!!");
+                            // if (a.Contains(' ')) throw new Exception("!!!");
+                            args.Add(a);
+                        }
+                    }
+
+                    return new Arguments(args, kwargs);
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ VGPrompter/Script/Parser/Script.Parser.Lexer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace VGPrompter {
+
+    public partial class Script {
+
+        public static partial class Parser {
+
+            const string
+                ARGS_PH = "%a",
+                STR_PH = "%s";
+
+            static readonly Regex
+                ARGS_RE = new Regex(@"\((.*)\)", RegexOptions.Compiled),
+                STRING_RE = new Regex(@"""(.*)""", RegexOptions.Compiled);
+
+            enum TokenType {
+                Text,
+                ArgumentList,
+                Identifier,
+                Comma,
+                If,
+                ElIf,
+                Else,
+                While,
+                Jump,
+                Call,
+                Label,
+                Return,
+                Define,
+                Menu
+            }
+
+            struct Token {
+                public TokenType Type { get; private set; }
+                public object Value { get; private set; }
+
+                public Token(TokenType type, object value = null) : this() {
+                    Type = type;
+                    Value = value;
+                }
+            }
+
+            static TokenType GetTokenType(string s) {
+                switch (s) {
+                    case ARGS_PH:
+                        return TokenType.ArgumentList;
+                    case STR_PH:
+                        return TokenType.Text;
+                    case IF:
+                        return TokenType.If;
+                    case ELIF:
+                        return TokenType.ElIf;
+                    case ELSE:
+                        return TokenType.Else;
+                    case WHILE:
+                        return TokenType.While;
+                    case JUMP:
+                        return TokenType.Jump;
+                    case CALL:
+                        return TokenType.Call;
+                    case LABEL:
+                        return TokenType.Label;
+                    case RETURN:
+                        return TokenType.Return;
+                    case DEFINE:
+                        return TokenType.Define;
+                    case MENU:
+                        return TokenType.Menu;
+                    default:
+                        if (!Arguments.identifier_re.Match(s).Success)
+                            throw new Exception(string.Format("Invalid token '{0}'", s));
+                        return TokenType.Identifier;
+                }
+            }
+
+            static Token[] Tokenize(string line) {
+
+                var s = line.Trim();
+
+                var trailing_comma = s[s.Length - 1] == ':';
+                if (trailing_comma) {
+                    s = s.Substring(0, s.Length - 1);
+                }
+
+                var m = ARGS_RE.Match(s);
+                Arguments? args = null;
+                if (m.Success) {
+                    args = Arguments.Parse(m.Groups[1].Value);
+                    s = ARGS_RE.Replace(s, " " + ARGS_PH + " ");
+                }
+
+                m = STRING_RE.Match(s);
+                var text = "";
+                if (m.Success) {
+                    text = m.Groups[1].Value;
+                    s = STRING_RE.Replace(s, " " + STR_PH + " ");
+                }
+
+                var tokens = s.Split(' ')
+                    .Where(x => !string.IsNullOrEmpty(x));
+
+                var ptokens = new List<Token>();
+
+                foreach (var t in tokens) {
+                    var type = GetTokenType(t);
+                    switch (type) {
+                        case TokenType.Text:
+                            ptokens.Add(new Token(type, text));
+                            break;
+                        case TokenType.ArgumentList:
+                            ptokens.Add(new Token(type, args));
+                            break;
+                        case TokenType.Identifier:
+                            ptokens.Add(new Token(type, t));
+                            break;
+                        default:
+                            ptokens.Add(new Token(type));
+                            break;
+                    }
+                }
+
+                if (trailing_comma) {
+                    ptokens.Add(new Token(TokenType.Comma));
+                }
+
+                return ptokens.ToArray();
+            }
+
+        }
+
+    }
+
+}
